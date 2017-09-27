9194b20c2831b2df0e5ca607c0737897cbc8afd8
Bertrand Marron <bertrand.marron@gmail.com>
2017-09-27 16:57:26 +0200

    Use serve-static to serve files in tests

--- tests/__helpers__/server.js
+++ tests/__helpers__/server.js
@@ -1,15 +1,25 @@
 const http = require('http')
-const fs = require('fs')
+const path = require('path')
 const listen = require('test-listen')
+const serveStatic = require('serve-static')
+const finalhandler = require('finalhandler')
 
-function serveFile(location) {
-  const srv = http.createServer(
-    (req, res) => {
-      fs.createReadStream(location).pipe(res)
-    }
-  )
-
-  return listen(srv)
+function createServer(handler) {
+  return listen(http.createServer(handler))
 }
 
-module.exports = {serveFile}
+function serveDirectory(location, options) {
+  const serve = serveStatic(location, options)
+
+  return createServer((req, res) => {
+    serve(req, res, finalhandler(req, res))
+  })
+}
+
+function serveFile(location) {
+  return serveDirectory(path.dirname(location), {
+    index: path.basename(location)
+  })
+}
+
+module.exports = {serveFile, serveDirectory}
